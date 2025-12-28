4bdde1b590c4fe5e65f6b2f937b684f3bc03fab2
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-28 12:15:25 +0500

    najarildi

---
+++ task01.py
@@ -0,0 +1,9 @@
+people = [("Ali", 24), ("Laylo", 30), ("Jasur", 19)]
+
+oldest_person = people[0]
+
+for person in people:
+    if person[1] > oldest_person[1]:
+        oldest_person = person   
+
+print(oldest_person)
